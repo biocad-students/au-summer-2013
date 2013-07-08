a33f061794eefcd1f5a04b5cdadb609ea18e3438
Pavel Yakovlev <zhmactep@gmail.com>
2013-07-08 14:55:04 +0400

    Code review on figa_tools

--- lib/share/figa_tools.py
+++ lib/share/figa_tools.py
@@ -1,15 +1,15 @@
 import parse
 
 
-def _regionsortrule(s):
-	reglist = [i + str(j) for j in range(5) for i in ["FR", "CDR"]]
+def _region_sort_rule(s):
+	reglist = [i + str(j) for j in range(1,5) for i in ["FR", "CDR"]]
 	return reglist.index(s)
 
 
-def _parseheaderhelper(arg, header):
+def _parse_header_helper(arg, header):
 	try:
 		ig_arg = parse.search("%s={:w}" % arg, header).fixed[0]
-	except:
+	except (AttributeError, IndexError):
 		ig_arg = "Unknown"
 	return ig_arg
 
@@ -23,50 +23,50 @@ class FIGA(object):
 	ig_regions = {}
 	ig_additional = {}
 
-	def setname(self, newname):
+	def set_name(self, newname):
 		self.ig_name = newname
 
-	def getname(self):
+	def get_name(self):
 		return self.ig_name
 
-	def settype(self, newtype):
+	def set_type(self, newtype):
 		self.ig_type = newtype
 
-	def gettype(self):
+	def get_type(self):
 		return self.ig_type
 
-	def setalpha(self, newalpha):
+	def set_alpha(self, newalpha):
 		self.ig_alpha = newalpha
 
-	def getalpha(self):
+	def get_alpha(self):
 		return self.ig_alpha
 
-	def setspecie(self, newspecie):
+	def set_specie(self, newspecie):
 		self.ig_specie = newspecie
 
-	def getspecie(self):
+	def get_specie(self):
 		return self.ig_specie
 
-	def setseq(self, newseq):
+	def set_seq(self, newseq):
 		self.ig_seq = newseq
 
-	def getseq(self):
+	def get_seq(self):
 		return self.ig_seq
 
-	def setregion(self, regionname, region):
+	def set_region(self, regionname, region):
 		self.ig_regions[regionname] = region
 
-	def getregion(self, regionname):
+	def get_region(self, regionname):
 		return self.ig_regions[regionname]
 
-	def setadditional(self, additionalname, value):
+	def set_additional(self, additionalname, value):
 		self.ig_additional[additionalname] = value
 
-	def getadditional(self, additionalname):
+	def get_additional(self, additionalname):
 		return self.ig_additional[additionalname]
 
-	def serialize(self):
-		sorted_regions = sorted(self.ig_regions.items(), key=lambda x: _regionsortrule(x[0]))
+	def dumps(self):
+		sorted_regions = sorted(self.ig_regions.items(), key=lambda x: _region_sort_rule(x[0]))
 		result = [">%s@(type=%s, alpha=%s, specie=%s)" % (self.ig_name, self.ig_type, 
 														  self.ig_alpha, self.ig_specie)]
 		for region, value in sorted_regions:
@@ -76,66 +76,66 @@ class FIGA(object):
 		result.append(self.ig_seq)
 		return "\n".join(result)
 
-	def parseheader(self, header):
+	def parse_header(self, header):
 		header = "".join(header.split())
 		try:
 			self.ig_name = parse.search(">{:S}@", header).fixed[0]
-		except:
+		except (AttributeError, IndexError):
 			self.ig_name = "Unknown"
-		self.ig_type = _parseheaderhelper("type", header)
-		self.ig_alpha = _parseheaderhelper("alpha", header)
-		self.ig_specie = _parseheaderhelper("specie", header)
+		self.ig_type = _parse_header_helper("type", header)
+		self.ig_alpha = _parse_header_helper("alpha", header)
+		self.ig_specie = _parse_header_helper("specie", header)
 		
-	def parseseq(self, seq):
+	def parse_seq(self, seq):
 		self.ig_seq = seq
 
-	def parseregion(self, region):
+	def parse_region(self, region):
 		region = "".join(region.split())
 		try:
 			name = parse.search(";@{:w}:", region).fixed[0]
 			reg = parse.search(":({:d},{:d})", region).fixed
 			self.ig_regions[name] = reg
-		except:
+		except (AttributeError, IndexError):
 			pass
 
-	def parseadditional(self, additional):
+	def parse_additional(self, additional):
 		additional = "".join(additional.split())
 		try:
 			name = parse.search(";+{:w}:", additional).fixed[0]
 			add = parse.search(":{:w}", additional).fixed[0]
 			self.ig_additional[name] = add
-		except:
+		except (AttributeError, IndexError):
 			pass
 
 	def parse(self, string):
 		lines = string.splitlines(True)
 		header = lines[0]
-		self.parseheader(header)
+		self.parse_header(header)
 		for line in lines[1:]:
 			if line.startswith(";@"):
-				self.parseregion(line)
+				self.parse_region(line)
 			elif line.startswith(";+"):
-				self.parseadditional(line)
+				self.parse_additional(line)
 			else:
-				self.parseseq(line)
+				self.parse_seq(line)
 
 
 def test_generate_figa():
 	a = FIGA()
-	a.setname("test-figa")
-	a.settype("VH")
-	a.setspecie("llama")
-	a.setalpha("amino")
-	a.setseq("EVQLVQSGAEVKKPGESLKISCKGSGYSFTTYWLGWVRQMPGKGLDWIGIMSPVDSDIRYSPSFQGQVTMSV"
+	a.set_name("test-figa")
+	a.set_type("VH")
+	a.set_specie("llama")
+	a.set_alpha("amino")
+	a.set_seq("EVQLVQSGAEVKKPGESLKISCKGSGYSFTTYWLGWVRQMPGKGLDWIGIMSPVDSDIRYSPSFQGQVTMSV"
 			 "DKSITTAYLQWNSLKASDTAMYYCARRRPGQGYFDFWGQGTLVTVSSASTKGPSVFPLA")
-	a.setregion("FR1", (0, 29))
-	a.setregion("CDR1", (30, 34))
-	a.setregion("FR2", (35, 48))
-	a.setregion("CDR2", (49, 65))
-	a.setregion("FR3", (66, 95))
-	a.setregion("CDR3", (96, 107))
-	a.setregion("FR4", (108, len(a.getseq()) - 1))
-	return a.serialize()
+	a.set_region("FR1", (0, 29))
+	a.set_region("CDR1", (30, 34))
+	a.set_region("FR2", (35, 48))
+	a.set_region("CDR2", (49, 65))
+	a.set_region("FR3", (66, 95))
+	a.set_region("CDR3", (96, 107))
+	a.set_region("FR4", (108, len(a.get_seq()) - 1))
+	return a.dumps()
 
 
 def test_load_figa(s):
@@ -146,5 +146,5 @@ def test_load_figa(s):
 
 def test():
 	s = test_generate_figa()
-	t = test_load_figa(s).serialize()
+	t = test_load_figa(s).dumps()
 	return s == t
