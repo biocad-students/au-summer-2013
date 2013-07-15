095c2b06e5a543f5ac86cef4e107537d89a40838
Timofey Bondarev <bondarevts@gmail.com>
2013-07-15 11:52:34 +0400

    refactoring for svm regions

--- lib/regions/draft.py
+++ lib/regions/draft.py
@@ -1,19 +1,14 @@
+__author__ = 'bondarevts'
+
 num2regions = ["FR1", "CDR1", "FR2", "CDR2", "FR3", "CDR3", "FR4"]
 
 from sklearn import svm
-import numpy as np
-from itertools import chain
+
 
 def getAminoMatrix(alphabet):
     aminoMatrices = {}
     for i, c in enumerate(alphabet):
-        values = []
-        for j in range(len(alphabet)):
-            if i == j:
-                values.append(1)
-            else:
-                values.append(0)
-        aminoMatrices[c] = values
+        aminoMatrices[c] = [1 if i == j else 0 for j in range(len(alphabet))]
     aminoMatrices['-'] = [0] * len(alphabet)
     return aminoMatrices
 
@@ -36,38 +31,32 @@ def getLearnSequences(sequence, markup, radius, values):
     s = [values['-']] * (radius + 1)
     length = len(sequence)
     for i in range(radius):
-        if i < length:
-            s.append(values[sequence[i]])
-        else:
-            s.append(values['-'])
-    X = []
-    y = []
+        s.append(values[sequence[i] if i < length else '-'])
+    X, y = [], []
     for i in range(length):
         s = s[1:]
-        if i + radius < length:
-            s.append(values[sequence[i + radius]])
-        else:
-            s.append(values['-'])
+        s.append(values[sequence[i + radius] if i + radius < length else '-'])
         X.append(s)
         y.append(markup[i])
         print markup[i], s
     return X, y
 
-alphabet = "ACDEFGHIKLMNPQRSTVWY"
-matrix = getAminoMatrix(alphabet)
+if __name__ == "__main__":
+    alphabet = "ACDEFGHIKLMNPQRSTVWY"
+    matrix = getAminoMatrix(alphabet)
 
-for a, value in matrix.items():
-    print a, value
+    for a, value in matrix.items():
+        print a, value
 
-result   = "000000000000000000000000000000  1111111  22222222222222  3333333333333333333  444444444444444444444444444  55555"
-sequence = "QVQLVQSGAEVKKPGASVKVSCKASGYTFT  G--YYMH  WVRQAPGQGLEWMG  WINP--NSGGTNYAQKFQG  RVTMTRDTSISTAYMELSRLRSDDTAV  YYCAR"
+    result   = "000000000000000000000000000000  1111111  22222222222222  3333333333333333333  444444444444444444444444444  55555"
+    sequence = "QVQLVQSGAEVKKPGASVKVSCKASGYTFT  G--YYMH  WVRQAPGQGLEWMG  WINP--NSGGTNYAQKFQG  RVTMTRDTSISTAYMELSRLRSDDTAV  YYCAR"
 
-a, b = cleanLearningSequences(sequence, result)
-print a
-print b
-print
+    a, b = cleanLearningSequences(sequence, result)
+    print a
+    print b
+    print
 
-classifier = svm.SVC(kernel='rbf')
+    classifier = svm.SVC(kernel='rbf')
 
-X, y = getLearnSequences(a, b, 3, matrix)
-# classifier.fit(np.asarray(list(chain(*X))), np.asarray(y))
+    X, y = getLearnSequences(a, b, 3, matrix)
+    # classifier.fit(np.asarray(list(chain(*X))), np.asarray(y))
