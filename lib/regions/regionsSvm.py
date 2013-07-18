466afa14b0e8430e8a1544a218a43162b1b74814
bondarevts <bondarevts@gmail.com>
2013-07-18 18:03:17 +0400

    fix svm and add simple filter svm results

--- lib/regions/regionsSvm.py
+++ lib/regions/regionsSvm.py
@@ -1,34 +1,36 @@
 import csv
 from itertools import chain
+from os import path
 
 from Bio import SeqIO
 from sklearn import svm
+from sklearn.externals import joblib
 import numpy as np
-
-__author__ = 'bondarevts'
 
 
 class Regions(object):
-    def __init__(self, alphabet, radius=11):
+    def __init__(self, alphabet, radius=11, dump=None):
         self.regions = {}
         self.init_symbol_vectors(alphabet)
         self.region_names = ["FR1", "CDR1", "FR2", "CDR2", "FR3", "CDR3", "FR4"]
         self.radius = radius
-        self.classifier = svm.SVC(kernel='rbf')
+        self.classifier = svm.SVC(kernel='rbf') if dump is None else joblib.load(dump)
 
     def learn_on_file(self, kabatPath, fastaPath):
         self.load_kabat(kabatPath)
         with open(fastaPath, "rU") as handle:
-            i = 0
-            for record in SeqIO.parse(handle, "fasta"):
-                i += 1
-                keys = self.create_dataset(record.seq)
+            learn_dataset, learn_results = ([], [])
+            for i, record in enumerate(SeqIO.parse(handle, "fasta")):
                 if not record.id in self.regions:
                     continue
+                keys = self.create_dataset(record.seq)
                 results = self.markup_dataset(keys, self.regions[record.id])
-                self.classifier.fit(np.asarray(keys, float), np.asarray(results, float))
-                if i % 1000 == 0:
-                    print i
+                learn_dataset.extend(keys)
+                learn_results.extend(results)
+            self.classifier.fit(np.asarray(learn_dataset, float), np.asarray(learn_results, float))
+
+    def dump(self, filename):
+        joblib.dump(self.classifier, filename, compress=9)
 
     def predict(self, gene):
         dataset = self.create_dataset(gene)
@@ -36,25 +38,21 @@ class Regions(object):
 
     def create_dataset(self, sequence):
         data = [self.symbol_vectors['-']] * (self.radius + 1)
-        length = len(sequence)
-        for i in range(self.radius):
-            data.append(self.symbol_vectors[sequence[i] if i < length else '-'])
+        seq_len = len(sequence)
+        for i in xrange(self.radius):
+            data.append(self.symbol_vectors[sequence[i] if i < seq_len else '-'])
         dataset = []
-        for i in range(length):
+        for i in xrange(seq_len):
             data = data[1:]
-            data.append(self.symbol_vectors[sequence[i + self.radius] if i + self.radius < length else '-'])
+            data.append(self.symbol_vectors[sequence[i + self.radius] if i + self.radius < seq_len else '-'])
             dataset.append(list(chain(*data)))
         return dataset
 
     def markup_dataset(self, dataset, regions):
-        results = []
-        for i in range(len(dataset)):
-            number = self.get_region_number(regions, i)
-            results.append(number)
-        return results
+        return [self.get_region_number(regions, i) for i in xrange(len(dataset))]
 
     def get_region_number(self, regions, position):
-        for i in range(len(regions) / 2):
+        for i in xrange(len(regions) / 2):
             if regions[i * 2] <= position + 1 <= regions[i * 2 + 1]:
                 return i
         raise KeyError("Too large position: {}. Regions end on: {}".format(position, regions[len(regions) - 1]))
@@ -67,13 +65,28 @@ class Regions(object):
     def init_symbol_vectors(self, alphabet):
         self.symbol_vectors = {}
         for i, c in enumerate(alphabet):
-            self.symbol_vectors[c] = [1 if i == j else 0 for j in range(len(alphabet))]
+            self.symbol_vectors[c] = [1 if i == j else 0 for j in xrange(len(alphabet))]
         self.symbol_vectors['-'] = [0] * len(alphabet)
+        self.symbol_vectors['N'] = [1] * len(alphabet)
         return self.symbol_vectors
 
 
 if __name__ == "__main__":
-    regions = Regions("ACGTN", 22)
-    regions.learn_on_file(r"..\..\data\nomenclature\human\VJL_combinations.kabat",
-                          r"..\..\data\germline\human\VJL_combinations.fasta")
+    alphabet = "ACGT"
+    radius = 40
+    dump_filename = "classifier.dmp"
+
+    regions = Regions(alphabet, radius)
+    regions.learn_on_file(path.abspath(r"..\..\data\nomenclature\human\VJL_combinations.kabat"),
+                          path.abspath(r"..\..\data\germline\human\VJL_combinations.fasta"))
     print regions.predict("CAGTCTGTGCTGACTCAGCCACCCTCGGTGTCTGAAGCCCCCAGGCAGAGGGTCACCATCTCCTGTTCTGGAAGCAGCTCCAACATCGGAAATAATGCTGTAAACTGGTACCAGCAGCTCCCAGGAAAGGCTCCCAAACTCCTCATCTATTATGATGATCTGCTGCCCTCAGGGGTCTCTGACCGATTCTCTGGCTCCAAGTCTGGCACCTCAGCCTCCCTGGCCATCAGTGGGCTCCAGTCTGAGGATGAGGCTGATTATTACTGTGCAGCATGGGATGACAGCCTGAATGGTCCTGCTGTGTTCGGAGGAGGCACCCAGCTGACCGTCCTCG")
+    regions.dump(dump_filename)
+
+    regions = Regions(alphabet, radius, dump_filename)
+    print "-" * 50
+    with open(path.abspath("VL_corrected.fasta"), "rU") as handle:
+        for i, record in enumerate(SeqIO.parse(handle, "fasta")):
+            print regions.predict(record.seq)
+            if i > 10:
+                break
+    # print regions.predict("CAGTCTGTGCTGACTCAGCCACCCTCGGTGTCTGAAGCCCCCAGGCAGAGGGTCACCATCTCCTGTTCTGGAAGCAGCTCCAACATCGGAAATAATGCTGTAAACTGGTACCAGCAGCTCCCAGGAAAGGCTCCCAAACTCCTCATCTATTATGATGATCTGCTGCCCTCAGGGGTCTCTGACCGATTCTCTGGCTCCAAGTCTGGCACCTCAGCCTCCCTGGCCATCAGTGGGCTCCAGTCTGAGGATGAGGCTGATTATTACTGTGCAGCATGGGATGACAGCCTGAATGGTCCTGCTGTGTTCGGAGGAGGCACCCAGCTGACCGTCCTCG")
